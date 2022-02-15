6b6804a58b97fe9b659b5a92452977663b5c88a7
Sixtine Caspar <sixcaspar@gmail.com>
2022-02-15 15:23:22 +0100

    add tests in rails

---
+++ spec/helpers/posts_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+include PostsHelper
+
+RSpec.describe PostsHelper, type: :helper do
+  it 'assigns a user to a post' do
+    creator = User.first_or_create!(email: 'dean@example.com', password: 'password', password_confirmation: 'password')
+    @post = Post.new(
+      title: 'MyString',
+      body: 'MyText',
+      views: 1
+    )
+    assign_post_creator(@post, creator)
+    expect(@post.user).to be(creator)
+  end
+end
